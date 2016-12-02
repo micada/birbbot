8fbc2a099556cf30961ae8ebd7b6c608762c3a23
Mica Alaniz <malaniz@leapfrogonline.com>
2016-12-02 15:59:25 -0600

    starting this up, borken else if in slack_bot

--- slack_bot.js
+++ slack_bot.js
@@ -1,246 +1,199 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
+'use strict';
 
+var firebase = require("firebase");
+var async = require('async');
+var moment = require('moment-timezone');
+var escapeStringRegexp = require('escape-string-regexp');
+var botkit = require('botkit');
+require('dotenv').config();
 
-This is a sample Slack bot built with Botkit.
+var today = moment().format().substr(0,10);
 
-This bot demonstrates many of the core features of Botkit:
+var config = {
+  apiKey: process.env.FB_API,
+  authDomain: process.env.FB_AUTH,
+  databaseURL: process.env.FB_URL
+};
 
-* Connect to Slack using the real time API
-* Receive messages based on "spoken" patterns
-* Reply to messages
-* Use the conversation system to ask questions
-* Use the built in storage system to store and retrieve information
-  for a user.
+firebase.initializeApp(config);
 
-# RUN THE BOT:
+var collection = firebase.database().ref(today);
 
-  Get a Bot token from Slack:
-
-    -> http://my.slack.com/services/new/bot
-
-  Run your bot from the command line:
-
-    token=<MY TOKEN> node slack_bot.js
-
-# USE THE BOT:
-
-  Find your bot inside Slack to send it a direct message.
-
-  Say: "Hello"
-
-  The bot will reply "Hello!"
-
-  Say: "who are you?"
-
-  The bot will tell you its name, where it is running, and for how long.
-
-  Say: "Call me <nickname>"
-
-  Tell the bot your nickname. Now you are friends.
-
-  Say: "who am I?"
-
-  The bot will tell you your nickname, if it knows one for you.
-
-  Say: "shutdown"
-
-  The bot will ask if you are sure, and then shut itself down.
-
-  Make sure to invite your bot into other channels using /invite @<my bot>!
-
-# EXTEND THE BOT:
-
-  Botkit has many features for building cool and useful bots!
-
-  Read all about it here:
-
-    -> http://howdy.ai/botkit
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-
-if (!process.env.token) {
-    console.log('Error: Specify token in environment');
-    process.exit(1);
+function connectToDb() {
+  startBot(collection);
 }
 
-var Botkit = require('./lib/Botkit.js');
-var os = require('os');
+function startBot() {
 
-var controller = Botkit.slackbot({
-    debug: true,
-});
+  // collection.remove();
 
-var bot = controller.spawn({
-    token: process.env.token
-}).startRTM();
+  var botkitController = botkit.slackbot({
+    debug: true
+  });
 
-
-controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    bot.api.reactions.add({
-        timestamp: message.ts,
-        channel: message.channel,
-        name: 'robot_face',
-    }, function(err, res) {
-        if (err) {
-            bot.botkit.log('Failed to add emoji reaction :(', err);
-        }
-    });
+  botkitController.spawn({
+    token: process.env.TOKEN
+  }).startRTM();
 
 
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Hello ' + user.name + '!!');
-        } else {
-            bot.reply(message, 'Hello.');
-        }
-    });
-});
+  botkitController.on('reaction_added', function(bot, event) {
+                bot.botkit.log('TOP\n\n\n\n', event);
+    var stamp = event.item.ts.split('.').join('');
 
-controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
-    var name = message.match[1];
-    controller.storage.users.get(message.user, function(err, user) {
-        if (!user) {
-            user = {
-                id: message.user,
-            };
-        }
-        user.name = name;
-        controller.storage.users.save(user, function(err, id) {
-            bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-        });
-    });
-});
+    if (event.reaction==='heart') {
+      var birbCollection = collection.child(stamp);
 
-controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
+      birbCollection.once('value', function(snapshot) {
+        var birb = 1;
 
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Your name is ' + user.name);
-        } else {
-            bot.startConversation(message, function(err, convo) {
-                if (!err) {
-                    convo.say('I do not know your name yet!');
-                    convo.ask('What should I call you?', function(response, convo) {
-                        convo.ask('You want me to call you `' + response.text + '`?', [
-                            {
-                                pattern: 'yes',
-                                callback: function(response, convo) {
-                                    // since no further messages are queued after this,
-                                    // the conversation will end naturally with status == 'completed'
-                                    convo.next();
-                                }
-                            },
-                            {
-                                pattern: 'no',
-                                callback: function(response, convo) {
-                                    // stop the conversation. this will cause it to end with status == 'stopped'
-                                    convo.stop();
-                                }
-                            },
-                            {
-                                default: true,
-                                callback: function(response, convo) {
-                                    convo.repeat();
-                                    convo.next();
-                                }
-                            }
-                        ]);
-
-                        convo.next();
-
-                    }, {'key': 'nickname'}); // store the results in a field called nickname
-
-                    convo.on('end', function(convo) {
-                        if (convo.status == 'completed') {
-                            bot.reply(message, 'OK! I will update my dossier...');
-
-                            controller.storage.users.get(message.user, function(err, user) {
-                                if (!user) {
-                                    user = {
-                                        id: message.user,
-                                    };
-                                }
-                                user.name = convo.extractResponse('nickname');
-                                controller.storage.users.save(user, function(err, id) {
-                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-                                });
-                            });
-
-
-
-                        } else {
-                            // this happens if the conversation ended prematurely for some reason
-                            bot.reply(message, 'OK, nevermind!');
-                        }
-                    });
-                }
+        if (snapshot.val() === null ) {
+          bot.api.channels.history({
+            channel: event.item.channel,
+            latest: event.item.ts,
+            count: 1,
+            inclusive: 1
+          }, function(err, response) {
+            bot.botkit.log('WHY\n\n\n\n', response);
+            birbCollection.update({
+              id: response.latest,
+              text: response.messages[0].text,
+              user: response.messages[0].user,
+              birbScore: birb,
+              birbs: new Array.push(event.user)
             });
+          });
+        } else if (snapshot.val().birbs.includes(event.user)) {
+          // new user can't vote
+          return;
+        } else {
+          birbCollection.child("/score").transaction(function(score) {
+            score = score + birb;
+            return score;
+          });
         }
-    });
-});
-
-
-controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    bot.startConversation(message, function(err, convo) {
-
-        convo.ask('Are you sure you want me to shutdown?', [
-            {
-                pattern: bot.utterances.yes,
-                callback: function(response, convo) {
-                    convo.say('Bye!');
-                    convo.next();
-                    setTimeout(function() {
-                        process.exit();
-                    }, 3000);
-                }
-            },
-        {
-            pattern: bot.utterances.no,
-            default: true,
-            callback: function(response, convo) {
-                convo.say('*Phew!*');
-                convo.next();
-            }
-        }
-        ]);
-    });
-});
-
-
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
-    'direct_message,direct_mention,mention', function(bot, message) {
-
-        var hostname = os.hostname();
-        var uptime = formatUptime(process.uptime());
-
-        bot.reply(message,
-            ':robot_face: I am a bot named <@' + bot.identity.name +
-             '>. I have been running for ' + uptime + ' on ' + hostname + '.');
-
-    });
-
-function formatUptime(uptime) {
-    var unit = 'second';
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'minute';
+      });
     }
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'hour';
-    }
-    if (uptime != 1) {
-        unit = unit + 's';
-    }
+  });
 
-    uptime = uptime + ' ' + unit;
-    return uptime;
+  // botkitController.hears(['report'], ['direct_message', 'direct_mention'], function(bot, message) {
+  //   bot.reply(message, "Let me make sure everyone's dude status has been set.");
+
+  //   function assignDude(status, val) {
+  //     collection.child('members/' + val.id).once('value', function(snapshot) {
+  //       collection.child('members/' + val.id).update({dudeStatus: status});
+  //       bot.reply(message, 'OK! I will update my dossier for ' + '<@' + val.id + '>');
+  //     });
+  //   }
+
+  //   function askDude(val, convo) {
+  //     convo.ask('Is ' + '<@' + val.id + '>' + ' a dude?', [
+  //       {
+  //         pattern: bot.utterances.yes,
+  //         callback: function(response, convo) {
+  //           assignDude(true, val);
+  //           convo.next();
+  //         }
+  //       },
+  //       {
+  //         pattern: bot.utterances.no,
+  //         callback: function(response, convo) {
+  //           assignDude(false, val);
+  //           convo.next();
+  //         }
+  //       },
+  //       {
+  //         default: true,
+  //         callback: function(response,convo) {
+  //           convo.say("No problem! I'll ask again and you can just say " + '<@' + val.id + '>' + ' is not a dude.');
+  //           convo.repeat();
+  //           convo.next();
+  //         }
+  //       }
+  //     ]);
+  //     convo.next();
+  //   }
+
+  //   function getReport(convo) {
+  //     function getTotals(status) {
+
+  //       var indTotal = 0;
+
+  //       collection.child('members').orderByChild('dudeStatus')
+  //       .equalTo(status).on('value', function(snapshot) {
+  //         if (snapshot.val()) {
+  //           var dudes = Object.keys(snapshot.val()).map(function(key) {
+  //             return snapshot.val()[key];
+  //           });
+
+  //           dudes.forEach(function(usr, i, arr) {
+  //             indTotal += usr.score
+  //           });
+  //         }
+  //       });
+  //       return indTotal;
+  //     }
+
+  //     var dudeTotal = getTotals(true);
+  //     var notDudeTotal = getTotals(false);
+  //     var percent = (notDudeTotal !== 0) ? Math.trunc((dudeTotal / (dudeTotal + notDudeTotal)) * 100) : 0;
+
+  //     collection.child('totals').update(
+  //       {
+  //         dudeTotal: dudeTotal,
+  //         notDudeTotal: notDudeTotal,
+  //         percentageDude: percent
+  //       }
+  //     );
+
+  //     collection.child('totals').once('value').then(function(snapshot) {
+  //       if ((dudeTotal && notDudeTotal) === 0) {
+  //         bot.reply(message, 'Not enough talking has gone on! Maybe try again later.');
+  //       } else {
+  //         bot.reply(message, "All dude statuses are in!");
+
+
+  //         bot.botkit.log('report total log', dudeTotal + notDudeTotal);
+  //         bot.botkit.log('report result log', percent);
+
+  //         bot.reply(message, 'Dudes have talked:\n' + percent + '% of the time');
+  //       }
+  //     });
+  //   }
+
+
+  //   function talkedToday(convo) {
+  //     function checkDude(snapshot) {
+  //       async.everySeries(snapshot.val(), function(val, cb, err) {
+  //         if (val.dudeStatus === 0) {
+  //           askDude(val, convo);
+  //         } else {
+  //           cb(null, !err);
+  //         }
+  //       }, function(err) {
+  //         if (err) {
+  //           bot.reply(message, 'Something went wrong. Try again?');
+  //         } else {
+  //           getReport(convo);
+  //           convo.stop();
+  //           collection.child('members').off('value', checkDude);
+  //         }
+  //       });
+  //     }
+
+  //     collection.child('members').on('value', checkDude);
+  //   }
+
+    // bot.startConversation(message, function(err, convo) {
+    //   talkedToday(convo);
+    // })
+
+  // });
+
 }
+
+if (!process.env.TOKEN) {
+  console.log('Error: Specify token in environment');
+  process.exit(1);
+}
+
+connectToDb();
