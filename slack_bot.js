187926dd6e630fa4799689dac16027424a145f97
Mica Alaniz <malaniz@leapfrogonline.com>
2017-01-05 16:43:34 -0600

    fixing some stuff

--- slack_bot.js
+++ slack_bot.js
@@ -37,14 +37,14 @@ function startBot() {
 
 
   botkitController.on('reaction_added', function(bot, event) {
-                bot.botkit.log('TOP\n\n\n\n', event);
-    var stamp = event.item.ts.split('.').join('');
+    bot.botkit.log('TOP\n\n\n\n', event.user);
+    const stamp = event.item.ts.split('.').join('');
 
     if (event.reaction==='heart') {
-      var birbCollection = collection.child(stamp);
+      const birbCollection = collection.child(stamp);
+      const birbGiver = event.user;
 
       birbCollection.once('value', function(snapshot) {
-        var birb = 1;
 
         if (snapshot.val() === null ) {
           bot.api.channels.history({
@@ -58,137 +58,57 @@ function startBot() {
               id: response.latest,
               text: response.messages[0].text,
               user: response.messages[0].user,
-              birbScore: birb,
-              birbs: new Array.push(event.user)
+              birbScore: 1,
+              birbs: [
+                {event.user : true}
+              ]
             });
           });
-        } else if (snapshot.val().birbs.includes(event.user)) {
-          // new user can't vote
-          return;
-        } else {
+        }
+
+        birbCollection.transaction(function(birb) {
+          if (birb) {
+            if (birb.birbs && birb.birbs[event.user]) {
+              birb.birbScore--;
+              birb.birbs[event.user] = null;
+            } else {
+              birb.birbScore++;
+              if (!birb.birbs) {
+                birb.birbs = {};
+              }
+              birb.birbs[event.user] = true;
+            }
+          }
+          return birb;
+        });
+
+      });
+    }
+  });
+
+  botkitController.on('reaction_removed', function(bot, event) {
+    bot.botkit.log('TOP\n\n\n\n', event.user);
+    var stamp = event.item.ts.split('.').join('');
+
+    if (event.reaction==='heart') {
+      var birbCollection = collection.child(stamp);
+
+      birbCollection.once('value', function(snapshot) {
+
+        bot.botkit.log('SNAP\n\n\n\n\n\n\nSHOT\n\n\n\n+++++++++++++++', snapshot.val().birbs);
+        var birbGivers = snapshot.val().birbs.indexOf(event.user);
+        if (birbGivers > -1) {
           birbCollection.child("/score").transaction(function(score) {
-            score = score + birb;
+            score = score - 1;
             return score;
+          });
+          birbCollection.update({
+            birbs: snapshot.val().birbs.splice(birbGivers, 1)
           });
         }
       });
     }
   });
-
-  // botkitController.hears(['report'], ['direct_message', 'direct_mention'], function(bot, message) {
-  //   bot.reply(message, "Let me make sure everyone's dude status has been set.");
-
-  //   function assignDude(status, val) {
-  //     collection.child('members/' + val.id).once('value', function(snapshot) {
-  //       collection.child('members/' + val.id).update({dudeStatus: status});
-  //       bot.reply(message, 'OK! I will update my dossier for ' + '<@' + val.id + '>');
-  //     });
-  //   }
-
-  //   function askDude(val, convo) {
-  //     convo.ask('Is ' + '<@' + val.id + '>' + ' a dude?', [
-  //       {
-  //         pattern: bot.utterances.yes,
-  //         callback: function(response, convo) {
-  //           assignDude(true, val);
-  //           convo.next();
-  //         }
-  //       },
-  //       {
-  //         pattern: bot.utterances.no,
-  //         callback: function(response, convo) {
-  //           assignDude(false, val);
-  //           convo.next();
-  //         }
-  //       },
-  //       {
-  //         default: true,
-  //         callback: function(response,convo) {
-  //           convo.say("No problem! I'll ask again and you can just say " + '<@' + val.id + '>' + ' is not a dude.');
-  //           convo.repeat();
-  //           convo.next();
-  //         }
-  //       }
-  //     ]);
-  //     convo.next();
-  //   }
-
-  //   function getReport(convo) {
-  //     function getTotals(status) {
-
-  //       var indTotal = 0;
-
-  //       collection.child('members').orderByChild('dudeStatus')
-  //       .equalTo(status).on('value', function(snapshot) {
-  //         if (snapshot.val()) {
-  //           var dudes = Object.keys(snapshot.val()).map(function(key) {
-  //             return snapshot.val()[key];
-  //           });
-
-  //           dudes.forEach(function(usr, i, arr) {
-  //             indTotal += usr.score
-  //           });
-  //         }
-  //       });
-  //       return indTotal;
-  //     }
-
-  //     var dudeTotal = getTotals(true);
-  //     var notDudeTotal = getTotals(false);
-  //     var percent = (notDudeTotal !== 0) ? Math.trunc((dudeTotal / (dudeTotal + notDudeTotal)) * 100) : 0;
-
-  //     collection.child('totals').update(
-  //       {
-  //         dudeTotal: dudeTotal,
-  //         notDudeTotal: notDudeTotal,
-  //         percentageDude: percent
-  //       }
-  //     );
-
-  //     collection.child('totals').once('value').then(function(snapshot) {
-  //       if ((dudeTotal && notDudeTotal) === 0) {
-  //         bot.reply(message, 'Not enough talking has gone on! Maybe try again later.');
-  //       } else {
-  //         bot.reply(message, "All dude statuses are in!");
-
-
-  //         bot.botkit.log('report total log', dudeTotal + notDudeTotal);
-  //         bot.botkit.log('report result log', percent);
-
-  //         bot.reply(message, 'Dudes have talked:\n' + percent + '% of the time');
-  //       }
-  //     });
-  //   }
-
-
-  //   function talkedToday(convo) {
-  //     function checkDude(snapshot) {
-  //       async.everySeries(snapshot.val(), function(val, cb, err) {
-  //         if (val.dudeStatus === 0) {
-  //           askDude(val, convo);
-  //         } else {
-  //           cb(null, !err);
-  //         }
-  //       }, function(err) {
-  //         if (err) {
-  //           bot.reply(message, 'Something went wrong. Try again?');
-  //         } else {
-  //           getReport(convo);
-  //           convo.stop();
-  //           collection.child('members').off('value', checkDude);
-  //         }
-  //       });
-  //     }
-
-  //     collection.child('members').on('value', checkDude);
-  //   }
-
-    // bot.startConversation(message, function(err, convo) {
-    //   talkedToday(convo);
-    // })
-
-  // });
-
 }
 
 if (!process.env.TOKEN) {
